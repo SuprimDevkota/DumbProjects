59c01ee23f623aca5f8c1661e022b1dd74ba818f
Suprim Devkota <80959795+SuprimDevkota@users.noreply.github.com>
2021-03-22 21:33:07 +0545

    Create CoL_comparision.py

---
+++ CoL_comparision.py
@@ -0,0 +1,25 @@
+
+
+# Building a webscraper to go fetch data from a website that contains the price difference between cost of commodities in two different places
+import requests
+from bs4 import BeautifulSoup
+url = 'https://www.numbeo.com/cost-of-living/compare_cities.jsp?country1=Nepal&country2=United+States&city1=Kathmandu&city2=Lynchburg%2C+VA'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, "html.parser")
+
+# Extracting only the required information from the soup
+difference = soup.find_all(style="vertical-align : middle; text-align: right")
+difference_list = []
+for item in difference:
+    difference_list.append(item.text.strip().rstrip("%"))
+  
+# Formatting the list properly to allow for arithmetic operations.
+diff_list = [i.replace(",", "") for i in difference_list]
+d_list = [float(x) for x in diff_list if x]
+
+# Taking the sum of the differences ranging from meal prices to rent and entertainment to get a general idea 
+sum = 0
+for item in range(len(d_list)):
+    sum += item
+
+print(sum)
